6984dbf8dd9ac80f45352b66d26016911c7960e4
Kushal Raja <kraja@beckman.com>
2020-09-07 15:43:42 +0530

    Add new moviestore project

---
+++ MovieStore.API/Controllers/NamesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NamesController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            return new[] { Environment.MachineName };
+        }
+    }
+}
---
+++ MovieStore/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MovieStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<IndexModel> _logger;
+
+        public string[] Names { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClient = httpClientFactory.CreateClient("PricingApiClient");
+        }
+
+        public async Task OnGet()
+        {
+            var response = await _httpClient.GetAsync("/api/names");
+            var content = await response.Content.ReadAsStringAsync();
+            Names = JsonSerializer.Deserialize<string[]>(content);
+        }
+    }
+}
